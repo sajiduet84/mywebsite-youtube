eb8c356b228a3677275cb20ef6f4662660048e55
Sajid Mahmood <Sajid.mahmood.bth@gmail.com>
2022-12-09 16:50:05 +0500

    Version 2

--- config.js
+++ config.js
@@ -2,3 +2,6 @@ console.log("version");
 console.log("version history");
 console.log("Welcome to HTML ocurse ");
 
+let a = 100;
+
+
--- login.js
+++ login.js
@@ -1,3 +1,6 @@
 console.log("sajid mamood");
 console.log("welcome to updates");
 
+let b = "string ";
+
+
